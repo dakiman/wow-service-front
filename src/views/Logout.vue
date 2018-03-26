b05beb82ef1e193b8c06eed17800f941df78f1fe
dakiman <dvancov@hotmail.com>
2018-03-27 00:30:26 +0200

    Logout functionality

--- src/views/Logout.vue
+++ src/views/Logout.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="home">
-    <h1>You have successfully logged out!</h1>
-    <h2>You will be redirected shortly.</h2>
+    <div id="placeholder">
+      <h1>Logging out...</h1> 
+    </div>
+    <div id="success" class="result-div ">
+      <h1>You have successfully logged out!</h1>
+      <h2>You will be redirected shortly.</h2>
+    </div>
+    <div id="failure" class="result-div">
+      <h1>There was an issue during logout.</h1>
+      <h2>Please wait a few minutes and try again.</h2>
+    </div>
   </div>
 </template>
 
@@ -13,15 +22,16 @@ export default {
           axios
             .post('http://127.0.0.1:8000/api/logout')
             .then(({data}) => {
+              document.getElementById('placeholder').style.visibility = 'visible';
+              document.getElementById('success').style.visibility = 'visible';
               auth.logout();
-              // setTimeout(this.$router.push('/'), 3000);
-              // this.$router.push('/')
               setTimeout(() => {
                 this.$router.push('/')
-              }, 3000)
+              }, 2000)
             })
             .catch(({response}) => {
-              // alert(response.data.message);
+              document.getElementById('placeholder').style.visibility = 'visible';              
+              document.getElementById('failure').style.visibility = 'visible';            
             })
         }
     },
@@ -36,5 +46,8 @@ export default {
     font-size: 14px;
     font-weight: lighter;
     color: grey;
+  }
+  .result-div {
+    visibility: hidden
   }
 </style>
